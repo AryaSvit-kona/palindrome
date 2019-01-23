3f217a15f7cc5146a8489ad393e6cd49677591ba
Marie <marie.n.ravet@gmail.com>
2019-01-23 14:39:20 +0100

    Problèmes Palindrome pour Pedro résolu avec Regex

--- BllPalindrome/PalindromeManagement.cs
+++ BllPalindrome/PalindromeManagement.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using DalPalindrome;
 
@@ -17,12 +18,18 @@ namespace BllPalindrome
 
             foreach (string line in readLines)
             {
-                string pLine; //Déclarer une nouvelle variable
-                string[] tableau; //Créer un tableau pour séparer les résultats en cellules
-                pLine = line.Replace(" ", ""); //Effacer tous les espaces
-                tableau = pLine.Split(',','(');// Sépare chaque "chose" à chaque ', '. Ca les met dans une nouvelle
+                string text = line;                
+                text = Regex.Replace(text, @"\(.*\)", "");// Remove text between brackets.
+                /*text = Regex.Replace(text, @"\s+", " ");*/                // Remove extra spaces.
+                //Console.WriteLine(text);
+                //string pLine; //Déclarer une nouvelle variable
+                string[] tableau;
+                text.Trim();
+                //Créer un tableau pour séparer les résultats en cellules
+               // pLine = line.Replace(" ", ""); //*
+                tableau = text.Split(',');// Sépare chaque "chose" à chaque ', '. Ca les met dans une nouvelle
                                            // cellule à chaque ','
-                cleanList.Add(tableau[0]);// On utilise uniquement la 1ere cellule et on le met dans le nouveau 
+                cleanList.Add(tableau[0].Trim());// On utilise uniquement la 1ere cellule et on le met dans le nouveau 
                                            // tableau qu'on a crée avant le foreach
                 
             }
